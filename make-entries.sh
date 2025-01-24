7da27249741210a3d241f9e01fdcef7184fc6b04
Richard <legalize@xmission.com>
2025-01-24 09:52:31 -0700

    Don't overwrite output file until finished

--- make-entries.sh
+++ make-entries.sh
@@ -1,12 +1,14 @@
 #!/usr/local/bin/bash
 find [12]??? -name '*.thumb.jpg' -print | sort > thumbs.txt
-rm -f entries.txt
+out=entries-tmp.txt
+rm -f $out
 for f in `cat thumbs.txt`; do
     par=${f/.thumb.jpg/.par}
     jpg=${f/.thumb.jpg/.jpg}
     if [ -e $par -a -e $jpg ]; then
-        printf "%s|%s|%s\n" $f $jpg $par >> entries.txt
+        printf "%s|%s|%s\n" $f $jpg $par >> $out
         echo $f
     fi
 done
 rm -f thumbs.txt
+mv -f $out entries.txt
